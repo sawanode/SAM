f2f38eda1afa8b9303afead19e92ab1ddaec52f1
sawanode <59454126+sawanode@users.noreply.github.com>
2020-02-07 22:00:12 +0300

    Update Run.lua

--- inc/Run.lua
+++ inc/Run.lua
@@ -196,7 +196,6 @@ print('\27[0;33m>>'..[[
 ]]..'\027[0;32m'
 ..'¦ TOKEN_BOT: \27[1;34m'..Token..'\027[0;32m\n'
 ..'¦ BOT__INFO: \27[1;34m'.. Bot_User..'\27[0;36m » ('..boss..')\027[0;32m\n'
-..'¦ INFO_SUDO: \27[1;34m'..SUDO_USER:gsub([[\_]],'_')..'\27[0;36m » ('..SUDO_ID..')\27[m\027[0;32m\n'
 ..'¦ Run_Scrpt: \27[1;34m./inc/Script.lua\027[0;32m \n'
 ..'¦ LOGIN__IN: \27[1;34m'..login..'\027[0;32m \n'
 ..'¦ VERSION->: \27[1;34mv'..version..'\027[0;32m\n'
